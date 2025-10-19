39bc5331098a26f2701bfbc159847595b2efbbd4
Cemal <c.igrek63@hotmail.com>
2025-10-19 17:08:09 +0300

    some features

--- src/main/java/com/example/Hospital/repository/PrescriptionRepository.java
+++ src/main/java/com/example/Hospital/repository/PrescriptionRepository.java
@@ -4,7 +4,10 @@ import com.example.Hospital.entity.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface PrescriptionRepository extends JpaRepository<Prescription,Long> {
 
-}
+    List<Prescription> findByAppointmentId(Long appointmentId);}

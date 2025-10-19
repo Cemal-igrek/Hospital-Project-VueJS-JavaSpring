3e9ee00a5fcc142f4a67e928e32a2609dacd5916
Cemal <c.igrek63@hotmail.com>
2025-10-20 02:13:28 +0300

    fix

--- src/main/java/com/example/Hospital/security/SecurityConfig.java
+++ src/main/java/com/example/Hospital/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.example.Hospital.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+@EnableMethodSecurity
+public class SecurityConfig{
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();

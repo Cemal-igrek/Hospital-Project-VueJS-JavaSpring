4f20c8dd47f539dea1a8b8d25010437436193e47
Cemal <c.igrek63@hotmail.com>
2025-10-22 02:57:02 +0300

    fix

--- frontend/hospital-frontend/src/services/apiService.js
+++ frontend/hospital-frontend/src/services/apiService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL:'http://localhost:8080/api'
 });
+apiClient.defaults.withCredentials = true;
 export default {
   // --- Auth Services ---
   login(credentials) {
